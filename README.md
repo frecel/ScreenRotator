4dacfb83d36e77933eba1d2881833046afd1dd05
frecel <aturfracek@gmail.com>
2016-01-01 18:38:50 -0700

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # ScreenRotator
 Easy applet for rotating the screen on Unity Desktop
+
+It requires python3-gi, xrandr and xbacklight. The first packages should already be installed by default if you are using Ubuntu 15.10
+
+The icon is from Google's Material Desing icon pack (https://materialdesignicons.com/)
---
+++ ScreenRotator.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+
+import os
+import signal
+from subprocess import call
+from gi.repository import Gtk
+from gi.repository import AppIndicator3 as AppIndicator
+
+APPINDICATOR_ID = "screenrotator"
+orientation = "normal"
+
+def main():
+    indicator = AppIndicator.Indicator.new(APPINDICATOR_ID, os.path.abspath('./icon.svg'), AppIndicator.IndicatorCategory.SYSTEM_SERVICES)
+    indicator.set_status(AppIndicator.IndicatorStatus.ACTIVE)
+    indicator.set_menu(build_menu())
+    Gtk.main()
+
+def build_menu():
+    menu = Gtk.Menu()
+    #brightness
+    item_brightness_up = Gtk.MenuItem('Increase Brightness')
+    item_brightness_up.connect('activate', increase_brightness)
+    menu.append(item_brightness_up)
+    item_brightness_down = Gtk.MenuItem("Decrease Brightness")
+    item_brightness_down.connect('activate', decrease_brightness)
+    menu.append(item_brightness_down)
+    #rotate
+    item_rotate = Gtk.MenuItem('Rotate')
+    item_rotate.connect('activate', rotate_screen)
+    menu.append(item_rotate)
+    #seperator
+    seperator = Gtk.SeparatorMenuItem()
+    menu.append(seperator)
+    #quit
+    item_quit = Gtk.MenuItem('Quit')
+    item_quit.connect('activate', quit)
+    menu.append(item_quit)
+    menu.show_all()
+    return menu
+
+def rotate_screen(source):
+    global orientation
+    if orientation == "normal":
+        direction = "left"
+    elif orientation == "left":
+        direction ="normal"
+    call(["xrandr", "-o", direction])
+    orientation = direction
+
+def increase_brightness(source):
+    call(["xbacklight", "-inc", "20"])
+
+def decrease_brightness(source):
+    call(["xbacklight", "-dec", "20"])
+
+def quit(source):
+    Gtk.main_quit()
+
+if __name__ == "__main__":
+    #make sure the screen is in normal orientation when the script starts
+    call(["xrandr", "-o", orientation])
+    #keyboard interrupt handler
+    signal.signal(signal.SIGINT, signal.SIG_DFL)
+    main()
