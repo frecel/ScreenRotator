c9301e33323154e7f3967e400d19defa67953959
henrysting <henrysting@hotmail.com>
2020-05-24 20:28:13 +0800

    update for MintLinux 19.3 (Ubuntu 18.04)

--- ScreenRotator.py
+++ ScreenRotator.py
@@ -2,19 +2,16 @@
 
 # Screen rotator and other useful switches for the tablet mode of Ubuntu/Mint Linux.
 # origially from https://github.com/frecel/ScreenRotator
-# Yu Heng (henrysting@gmail.com) modified and tested on Thinkpad X1 Yoga 
-# with Linux Mint 18.2 (kernel 4.10.0-38), Cinnamon 64-Bit
-# It should work for other laptops with the Wacom screen and pen. 
+# Yu Heng (henrysting@126.com) modified and tested on Thinkpad X1 Yoga 
+# with Linux Mint 19.3 (kernel 5.0.0-32), Cinnamon 64-Bit
+# It should also work for other laptops with the Wacom screen and stylus. 
 # https://github.com/henrysting/ScreenRotator
-#
-# Note: 
-# The screen rotating function is incompatible with the kernel 4.13.0-16. 
 #
 # Changelog: 
 # 2017-11-13: Start
 # 2018-02-09: add touchscreen on/off, onboard
 # 2018-02-10: add trackpoint on/off, snapshot
-# 
+# 2020.05.22: update xrandr command for Mint Linux 19.3
 
 import signal
 from subprocess import call, check_output
@@ -30,6 +27,7 @@ pen_id = ""
 finger_id = ""
 touchpad_id = ""
 trackpoint_id = ""
+screen_id = ""
 
 try:
     outext = check_output("xsetwacom --list devices | grep stylus | sed 's/.*id//' | awk '{print $2}'", shell=True)
@@ -49,6 +47,11 @@ try:
 except BaseException:
     print("TouchPad is not found. Please check the output of xinput.")
     
+try:
+    outext = check_output("xrandr | grep ' connected' | awk '{print $1}'", shell=True)
+    screen_id = outext.decode('ascii').strip()
+except BaseException:
+    print("Screen is not found. Please check the output of xrandr.")
     
 try:
     outext = check_output("xinput -list | grep TrackPoint | sed 's/.*id=//' | awk '{print $1}'", shell=True)
@@ -60,7 +63,8 @@ def main():
     import os        
 
     root=os.path.dirname(os.path.abspath(__file__))
-    indicator = AppIndicator.Indicator.new(APPINDICATOR_ID, root+'/icon_grey.svg', AppIndicator.IndicatorCategory.SYSTEM_SERVICES)
+    print(root)
+    indicator = AppIndicator.Indicator.new(APPINDICATOR_ID, root+'/icon.svg', AppIndicator.IndicatorCategory.SYSTEM_SERVICES)
     indicator.set_status(AppIndicator.IndicatorStatus.ACTIVE)
     indicator.set_menu(build_menu())
     Gtk.main()
@@ -88,16 +92,17 @@ def build_menu():
     seperator = Gtk.SeparatorMenuItem()
     menu.append(seperator)
         
-    #brightness
-    item_brightness_up = Gtk.MenuItem('Brightness +')
-    item_brightness_up.connect('activate', increase_brightness)
-    menu.append(item_brightness_up)
-    item_brightness_down = Gtk.MenuItem("Brightness -")
-    item_brightness_down.connect('activate', decrease_brightness)
-    menu.append(item_brightness_down)
-    #seperator
-    seperator = Gtk.SeparatorMenuItem()
-    menu.append(seperator)
+    ### battery tray contains the brightness control
+    #~ #brightness
+    #~ item_brightness_up = Gtk.MenuItem('Brightness +')
+    #~ item_brightness_up.connect('activate', increase_brightness)
+    #~ menu.append(item_brightness_up)
+    #~ item_brightness_down = Gtk.MenuItem("Brightness -")
+    #~ item_brightness_down.connect('activate', decrease_brightness)
+    #~ menu.append(item_brightness_down)
+    #~ #seperator
+    #~ seperator = Gtk.SeparatorMenuItem()
+    #~ menu.append(seperator)
     
     
     # onboard keyboard
@@ -159,28 +164,32 @@ def build_menu():
     return menu
 
 def reset_screen(source):
-    call(["xrandr", "-o", "normal"])
+    #~ call(["xrandr", "-o", "normal"])
+    call(["xrandr", "--output", screen_id, "--rotate", "normal"])
     if pen_id.isdigit():
         call(["xsetwacom", "set", pen_id, "rotate","none"])
     if finger_id.isdigit():
         call(["xsetwacom", "set", finger_id, "rotate","none"])
     
 def rotate_screen(source):
-    call(["xrandr", "-o", "left"])
+    #~ call(["xrandr", "-o", "left"])
+    call(["xrandr", "--output", screen_id, "--rotate", "left"])
     if pen_id.isdigit():
         call(["xsetwacom", "set", pen_id, "rotate","ccw"])
     if finger_id.isdigit():
         call(["xsetwacom", "set", finger_id, "rotate","ccw"])
 
 def rotate_screen_right(source):
-    call(["xrandr", "-o", "right"])
+    #~ call(["xrandr", "-o", "right"])
+    call(["xrandr", "--output", screen_id, "--rotate", "right"])
     if pen_id.isdigit():
         call(["xsetwacom", "set", pen_id, "rotate","cw"])
     if finger_id.isdigit():
         call(["xsetwacom", "set", finger_id, "rotate","cw"])
     
 def flip_screen(source):
-    call(["xrandr", "-o", "inverted"])
+    #~ call(["xrandr", "-o", "inverted"])
+    call(["xrandr", "--output", screen_id, "--rotate", "inverted"])
     if pen_id.isdigit():
         call(["xsetwacom", "set", pen_id, "rotate","half"])
     if finger_id.isdigit():
@@ -223,7 +232,8 @@ def decrease_brightness(source):
 
 if __name__ == "__main__":
     #make sure the screen is in normal orientation when the script starts
-    call(["xrandr", "-o", "normal"])
+    #~ call(["xrandr", "-o", "normal"])
+    call(["xrandr", "--output ",screen_id," --rotate", "normal"])
     if pen_id.isdigit():
         call(["xsetwacom", "set", pen_id, "rotate","none"])
     if finger_id.isdigit():
